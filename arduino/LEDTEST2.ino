e875b15e1d65d606a515e43ca939802058f3c581
akira345 <g.akira345@gmail.com>
2013-09-23 15:09:07 +0900

    バグを修正と、少しでも処理を軽くする為処理を最小限にする。

--- arduino/LEDTEST2.ino
+++ arduino/LEDTEST2.ino
@@ -12,61 +12,47 @@
 #include <MsTimer2.h>
 #include <Digit32x16.h>
 #define LED_PIN 13
-uint16_t kanji[16];
-uint16_t buf[16];
-unsigned int start_flg=0;
+uint16_t kanji[16];//ドット表示バッファ。16の整数倍を指定
 unsigned int len = 16;
-unsigned int cnt =0;
+unsigned int cnt = 0;
 void setup() {
   // put your setup code here, to run once:
   Digit32x16::Init();
   Serial.begin(9600);
-  for(unsigned int i=0;i<len;i++){
-    buf[i]=0x0000;
-    kanji[i]=0xffff;
-  } 
   pinMode(LED_PIN,OUTPUT);
 }
 
 void display_led(){
   digitalWrite(LED_PIN,LOW);  
-  for(int j=0; j < len; j+=16) {
-    for(unsigned int x=0;x<16;x++){
-      for(unsigned int y=0;y<16;y++){
-        unsigned int wk = kanji[j+y];
-        Digit32x16::Set(31,y,bitRead(wk,15-x));//縦16bit分
+  for(int j=0; j < len; j+=16) {               //文字数分ループ
+    for(unsigned int x=0;x<16;x++){            //横16bitループ
+      for(unsigned int y=0;y<16;y++){          //縦16bitループ
+        unsigned int wk = kanji[j+y];          //ドットデータ読み込み
+        Digit32x16::Set(31,y,bitRead(wk,15-x));//縦1bit分送信。x座標は右端31固定
       }
       delay(60);
-      Digit32x16::ShiftLeft();
+      Digit32x16::ShiftLeft();//出力したデータ全体を左に1bitシフトする
     }
   }
   digitalWrite(LED_PIN,HIGH);
-
-}
-void copy_display(){
-  for(unsigned int i=0;i<len;i++){  
-    kanji[i] = buf[i];
-  }
-  display_led();
 }
 void loop() {
   // put your main code here, to run repeatedly:
   //シリアルバッファにデータが有れば取り込む
   while(Serial.available()>1){
-    if (cnt>len){
-      cnt = 0;
-      start_flg=1;
-      copy_display();
+  //16個取り込んだらLEDへ表示する
+    if (cnt>len-1){
+      display_led();
+      //Serial.flush();
     } 
-
+ //シリアルデータがら上位8bit,下位8bitを取得し、16bitのデータを組み立てる
     unsigned int high_bit=Serial.read();//上位8bit
+    Serial.write(high_bit);
     unsigned int low_bit =Serial.read();//下位8bit
-    buf[cnt]=(high_bit<<8)+low_bit;//16bitに組み立て
+    Serial.write(low_bit);
+    //delay(1);
+    kanji[cnt]=(high_bit<<8)+low_bit;//16bitに組み立て
     cnt ++;
-  }
-  //初回以外は、バッファにたまっているデータを表示し続ける
-  if (start_flg){
-    display_led(); 
   }
 } 
 
